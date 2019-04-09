3bc531a90d087b2f9c692b55f2b9c2ebe24336f7
Jean Abayo <abagammy@gmail.com>
2019-04-09 10:53:18 +0300

    Basic login function
    Add json-web-token for auth
    
    Basic login function
    
    Add json-web-token for auth

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,6 @@
 const {
-	body,
-	validationResult,
+	validationResult
 } = require('express-validator/check');
-const {
-	sanitizeBody
-} = require('express-validator/filter');
 const bcrypt = require('bcrypt');
 
 const Users = require('../models/users');
@@ -12,22 +8,25 @@ const Users = require('../models/users');
 // Handles registering a user
 exports.registration = function(req, res) {
 	const errors = validationResult(req);
-	const saltRounds = process.env.BCRYPT_SALT_ROUNDS;
 	if (!errors.isEmpty()) {
 		return res.status(422).json({
 			errors: errors.array()
 		});
 	}
 
+	const saltRounds = process.env.BCRYPT_SALT_ROUNDS;
+
 	bcrypt.hash(req.body.password, parseInt(saltRounds))
 		.then(function(hash) {
-			Users.find({ username: req.body.username }, (error, user) => {
+			Users.find({
+				username: req.body.username
+			}, (error, user) => {
 				if (error) {
 					return res.status(400).json({
 						errors: error.message
 					})
 				}
-				if (user.length > 0) {
+				if (user && user.length > 0) {
 					return res.status(400).json({
 						errors: "User account already exists"
 					})
@@ -38,7 +37,7 @@ exports.registration = function(req, res) {
 					password: hash,
 				}).then(user => res.status(201).json(user));
 			});
-			
+
 		}).catch(function(error) {
 			return res.status(400).json({
 				errors: error.message
@@ -48,7 +47,34 @@ exports.registration = function(req, res) {
 
 // Handles logging in a user
 exports.login = function(req, res) {
-	res.send('NOT Implemented')
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(422).json({
+			errors: errors.array()
+		});
+	}
+
+	Users.findOne({
+		username: req.body.username
+	}, (error, user) => {
+		if (!user) {
+			return res.status(400).json({
+				errors: 'User does not exist',
+			})
+		}
+
+		bcrypt.compare(req.body.password, user.password, (err, passwordResponse) => {
+			if (!passwordResponse) {
+				return res.status(400).json({
+					errors: 'Invalid user credentials'
+				});
+			}
+			return res.status(200).json({
+				user: user,
+				message: 'success'
+			});
+		});
+	});
 }
 
 // Handles listing all users
